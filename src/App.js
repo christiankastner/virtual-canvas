2779c0759330d012f0196e2d73d3417ccc30eab9
ckastner12 <github email address>
2020-01-08 15:04:18 -0800

    Configuring Mo.js in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Burst from "./components/Burst"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  handleClick = e => {
+    console.log("I'm here")
+    Burst.tune({x: e.pageX, y: e.pageY})
+      .replay()
+  }
+  
+  render() {
+      return (
+        <div className="App" onClick={this.handleClick}>
+        Hello
+        </div>
+      );
+    }
+  }
 
 export default App;
---
+++ src/components/Burst.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import mojs from 'mo-js'
+
+
+const burst = new mojs.Burst({
+    left: 0, top: 0,
+    radius:   { 4: 19 },
+    angle:    45,
+    children: {
+      shape:        'line',
+      radius:       100,
+      scale:        2,
+      stroke:       '#FD7932',
+      strokeDasharray: '100%',
+      strokeDashoffset: { '-100%' : '100%' },
+      duration:     1000,
+       easing:       'quad.out',
+    }
+});
+
+export default burst
