160726192d856463f365491cac3e50a932dc3ec1
ckastner12 <github email address>
2020-01-09 17:03:27 -0800

    Creating Navbar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,31 @@ import CanvasShow from './CanvasShow'
 import { ActionCableProvider } from 'react-actioncable-provider';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import './App.css';
+import { Menu } from 'semantic-ui-react'
 import { API_WS_ROOT } from './constants';
 
 class App extends React.Component {
+
+  handleNavClick = (event) => {
+    console.log(event.target.name)
+  }
   
   render() {
       return (
+
         <ActionCableProvider url={API_WS_ROOT}>
           <Router >
+            <Menu >
+              <Menu.Item name="home" onClick={this.handleNavClick} >
+                Home
+              </Menu.Item>
+              <Menu.Item name="about" onClick={this.handleNavClick} >
+                About
+              </Menu.Item>
+              <Menu.Item name="canvases" onClick={this.handleNavClick} >
+                Canvases
+              </Menu.Item>
+            </Menu>
             <Route exact path="/" render={() => <Redirect to="/canvases" />} />
             <Route exact path="/canvases" render={() => <CanvasesIndex />} />
             <Route exact path="/canvases/:id" render={routerProps => (
--- src/containers/CanvasShowContainer.js
+++ src/containers/CanvasShowContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Canvas from '../components/Canvas'
-import { ActionCableConsumer } from 'react-actioncable-provider'
 
 class CanvasShowContainer extends React.Component {
     constructor(props) {
