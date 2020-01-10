006bb7e04efa665454d10beeaa3dcbd1494a74c3
ckastner12 <github email address>
2020-01-10 14:30:44 -0800

    Functionally sending information up cable and down to all clients

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import burst from "./Burst"
 import { ActionCableConsumer, ActionCableController } from 'react-actioncable-provider';
-import { API_ROOT, HEADERS } from '../constants/index'
+import { API_WS_ROOT, API_ROOT, HEADERS } from '../constants/index'
 const actioncable = require("actioncable")
 
 class Canvas extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            bursts: []
+            bursts: [],
         }
+    }
+
+    componentDidMount() {
+        this.cable = actioncable.createConsumer(API_WS_ROOT)
+        this.canvasChannel = this.cable.subscriptions.create({
+            channel: `PicturesChannel`, 
+            id: this.props.paramsId
+        },{
+            connected: () => console.log("pictureChannel connected"),
+            disconnected: () => console.log("pictureChannel disconnected"),
+            received: data => {
+            console.log(data)
+            this.handleRecievedBurst(data)
+            }
+        }
+        )
+
     }
 
     handleClick = e => {
         console.log("I clicked!")
+        this.canvasChannel.send({
+            canvas_id: this.props.paramsId,
+            loc_x: e.pageX,
+            loc_y: e.pageY
+        })
         
         // fetch(`${API_ROOT}/animate_mos`, {
         //   method: "POST",
@@ -29,7 +51,7 @@ class Canvas extends React.Component {
 
     handleRecievedBurst = response => {
         console.log(response)
-        const {loc_x, loc_y} = response.animate_mo
+        const {loc_x, loc_y} = response
         burst.tune({x: parseInt(loc_x), y: parseInt(loc_y)})
             .replay()
     }
