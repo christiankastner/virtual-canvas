6d2064b4a32cc734b59360cd3ba5e6e4714baede
ckastner12 <github email address>
2020-01-23 10:17:58 -0800

    deleting canvases working

--- src/containers/CreatedCanvasesContainer.js
+++ src/containers/CreatedCanvasesContainer.js
@@ -1,30 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import MaterialTable from 'material-table'
-import { API_ROOT } from '../constants/index'
+import React, { useState } from 'react'
+import Button from '@material-ui/core/Button'
+import { API_ROOT, HEADERS } from '../constants/index'
+
 
 const CreatedCanvasesContainer = props => {
     const [state, setState] = useState({
-        columns: [
-            {title: 'Title', field: 'title'},
-            {title: 'Creator', field: 'creator'},
-        ],
         data: [
             ...props.canvases.map(canvas => {
-                return {title: canvas.title, creator: canvas.user ? canvas.user.name : ''}
+                return {title: canvas.title, creator: canvas.user ? canvas.user.name : '', id: canvas.id}
             })
         ]
     })
 
+    const handleDelete = (id) => {
+        fetch(`${API_ROOT}/pictures/${id}`, {
+                            method: 'DELETE',
+                            headers: HEADERS
+                        })
+                            .then(resp => resp.json())
+                            .then(json => {
+                                if (json.message === "Successful") {
+                                    setState(prevState => {
+                                        return { data: [...prevState.data.filter(canvas => canvas.id !== id)]}
+                                    })
+                                }
+    
+                            })
+    }
+
+    const renderRows = () => {
+        return state.data.map(canvas => {
+            return (
+                <tr>
+                    <td><Button onClick={() => handleDelete(canvas.id)}>Delete</Button></td>   
+                    <td>{canvas.title}</td>
+                </tr>
+            )
+        })
+    }
+
     return (
-        <MaterialTable
-            title="Created Canvases"
-            columns={state.columns}
-            data={state.data}
-            editable={{
-                onRowDelete: oldData => {
-                    fetch(`${API_ROOT}/pictures`)
-                }
-            }} />
+        <div className="canvas-table">
+            <h3>Created Canvases</h3>
+            <table >
+                <tr>
+                    <th>Action</th>
+                    <th>Title</th>
+                </tr>
+                {renderRows()}
+            </table>
+        </div>
     )
 }
 
