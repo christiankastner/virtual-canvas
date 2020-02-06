7d5864c0ef793cc0aa52dc16587710b769e050c3
Christian Kastner <christianmkastner@gmail.com>
2020-02-06 12:35:48 -0800

    Updates

---
+++ precache-manifest.ea7868cecc9618ae3fdfdee3cff3d118.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86dd4dacf4b5880ef1cfb5dd320d7f51",
+    "url": "/virtual_canvas/index.html"
+  },
+  {
+    "revision": "95727b521b41e7eb1c35",
+    "url": "/virtual_canvas/static/css/main.d932b14c.chunk.css"
+  },
+  {
+    "revision": "0e2250b65fc3a78f285b",
+    "url": "/virtual_canvas/static/js/2.62d31cda.chunk.js"
+  },
+  {
+    "revision": "a2148be3dbd421a31f1137d4d2ca4573",
+    "url": "/virtual_canvas/static/js/2.62d31cda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95727b521b41e7eb1c35",
+    "url": "/virtual_canvas/static/js/main.c6e870c4.chunk.js"
+  },
+  {
+    "revision": "fe8cdc99a14ee617590e",
+    "url": "/virtual_canvas/static/js/runtime-main.8f3140f3.js"
+  },
+  {
+    "revision": "5deb96c1b84d8fec980a689c8144086e",
+    "url": "/virtual_canvas/static/media/dance-floor.5deb96c1.jpg"
+  },
+  {
+    "revision": "bc4e5cac85639f16f6dc29418709144b",
+    "url": "/virtual_canvas/static/media/folds.bc4e5cac.mp3"
+  },
+  {
+    "revision": "a713bd666b4f4aa248b2a9f5a6ff0449",
+    "url": "/virtual_canvas/static/media/lights.a713bd66.jpg"
+  }
+]);
