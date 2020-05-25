29b8796db37938b12111594317903fcf92bb5e4a
Christian Kastner <christianmkastner@gmail.com>
2020-05-25 13:39:32 -0700

    Onto login page

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import {ReactComponent as Logo} from '../../assets/header.svg'
 import './NavBar.scss'
 
 const Navbar = props => {
+
+    const [open, setOpen] = useState(false)
+
+    const handleMenuClick = () => {
+        setOpen(!open)
+    }
 
     const conditionalUserLink = () => {
         if (props.user_id) {
@@ -44,6 +50,11 @@ const Navbar = props => {
         <header >
             <Logo />
             <nav>
+            <svg class="menu-btn" id="menu-btn" viewBox="0 0 100 80" width="40" height="40" >
+                <rect width="100" height="20" fill="white"></rect>
+                <rect y="30" width="100" height="20" fill="white"></rect>
+                <rect y="60" width="100" height="20" fill="white"></rect>
+            </svg>
                 <ul>
                     <li>
                         <NavLink exact to="/" >
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,5 @@
+import React from "react"
+import "./Login.scss"
+
+
+const lo
